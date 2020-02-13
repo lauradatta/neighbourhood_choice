8efe0ffde6bb5c657f4a1caa09100a9993db4daf
Laura Datta <50656723+lauradatta@users.noreply.github.com>
2020-02-13 13:35:09 +0100

    Add files via upload

--- data-preparation/code/clean_data.R
+++ data-preparation/code/clean_data.R
@@ -1,13 +1,16 @@
 library("tidyverse")
 library("data.table")
 
-load('Data/bevolking18.Rdata')
-load('Data/verhuis1418.Rdata')
-load('Data/woningen18.Rdata')
+load('bevolking18.Rdata')
+load('verhuis1418.Rdata')
+load('woningen18.Rdata')
 
+
+## some EDA
 labels_prct19 <- c("huur < € 424" , "huur € 424-607", "huur € 607-720", "huur > € 720", "huur onbekend", 
                    "koop < € 185.000", "koop € 185-225.000", "koop € 225-275.000", "koop € 275-350.000", 
                    "koop € 350-425.000", "koop > € 425.000", "koop onbekend")  
+
 
 woningen18$prscat19 <- factor(woningen18$prscat19, levels = rev(labels_prct19))
 
