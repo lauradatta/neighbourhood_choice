c26fee3246ce8e2fe26a6026fbbd8aaf2105d617
laura.datta <laura@datta-online.com>
2020-04-03 16:29:17 +0200

    dataset for mlogit

--- src/analysis/mlogit_step1.R
+++ src/analysis/mlogit_step1.R
@@ -1,74 +1,121 @@
 library(data.table)
 library(tidyverse)
 
-source("src/analysis/nh_types.R")
+source("src/data-preparation/clean_data_nh.R")
 
-mod_var <- c("buurtnaam",
-        "PRSGES", 
-         "age", 
-         #"AANBEW", 
-         #"cdhhw", 
-         #"cbsetngr", 
-         "etnikort", 
-         #"vrgwrd", 
-         #"vrghr", 
-         #"vrgpct", 
-         #"vrgopp", 
-         #"vrgkam", 
-         "vrginh", 
-         #"vrghrkp", 
-         #"vrgeigd", 
-         #"vrgbjr", 
-         #"vrgbwwyz", 
-         #"vrgwtyp", 
-         #"vrgvlopp", 
-         #"AantalInwoners_5", 
-         #"HuishoudensTotaal_28", 
-        #"GemiddeldeWoningwaarde_35_vrg", 
-        #"Omgevingsadressendichtheid_106_vrg", 
-        #"AfstandTotZiekenhuis_11_vrg", 
-        #"AfstandTotOpritHoofdverkeersweg_89_vrg", 
-        #"Restaurants_3km_vrg", 
-        #"AfstandTotBelangrijkOverstapstation_91_vrg", 
-        #"AfstandTotKinderdagverblijf_52_vrg", 
-        #"AfstandTotBuitenschoolseOpvang_56_vrg", 
-        #"prop_nonwest_vrg", 
-        #"prop_singles_vrg", 
-        #"prop_couples_vrg", 
-        #"prop_fam_vrg", 
-        #"prop_bijstand_vrg", 
-        #"prop_income_socialmin_vrg", 
-        #"perc_property_vrg", 
-        #"perc_rent_vrg", 
-        #"perc_after2000_vrg", 
-        "cluster"
-        #"GemiddeldeWoningwaarde_35_nh", 
-        #"Omgevingsadressendichtheid_106_nh", 
-        #"AfstandTotZiekenhuis_11_nh", 
-        #"AfstandTotOpritHoofdverkeersweg_89_nh", 
-        #"Restaurants_3km_nh", 
-        #"AfstandTotBelangrijkOverstapstation_91_nh", 
-        #"AfstandTotKinderdagverblijf_52_nh", 
-        #"AfstandTotBuitenschoolseOpvang_56_nh", 
-        #"prop_nonwest_nh", 
-        #"prop_singles_nh", 
-        #"prop_couples_nh", 
-        #"prop_fam_nh", 
-        #"prop_bijstand_nh", 
-        #"prop_income_socialmin_nh", 
-        #"perc_property_nh", 
-        #"perc_rent_nh", 
-        #"perc_after2000_nh"
-)
-
-moves_18_log <- moves_18 %>%
-  select(mod_var)
-
-moves_18_log <- moves_18_log %>%
-  filter(!cluster %in% c( "outside", "neighbour mun"))%>%
+#for now only take nh of Den Bosch
+moves_db <- moves_db %>%
+  filter(!buurtnaam %in% c( "outside", "neighbour mun"))%>%
+  filter(jaar %in% c(2017, 2018))
   data.table
 
-wide <- moves_18_log %>%
-  mutate(var = 1) %>%
-  pivot_wider(names_from = cluster, values_from = var, names_prefix = "cluster_", values_fn = list(var = min), values_fill = list(var = 0))
+
+covar_indiv <- c("move_id",
+              "jaar",
+              "buurtnaam",
+             "buurtcode",
+             "vrg_buurtcode",
+              "PRSGES", 
+               "age", 
+               #"AANBEW", 
+               #"cdhhw", 
+               #"cbsetngr", 
+               "etnikort", 
+               #"vrgwrd", 
+               #"vrghr", 
+               #"vrgpct", 
+               #"vrgopp", 
+               #"vrgkam", 
+               "vrginh" 
+               #"vrghrkp", 
+               #"vrgeigd", 
+               #"vrgbjr", 
+               #"vrgbwwyz", 
+               #"vrgwtyp", 
+               #"vrgvlopp", 
+              )
+
+moves_subs <- moves_db %>%
+  select(covar_indiv) %>%
+  data.table
+
+#"AantalInwoners_5", 
+#"HuishoudensTotaal_28", 
+#"GemiddeldeWoningwaarde_35_vrg", 
+#"Omgevingsadressendichtheid_106_vrg", 
+#"AfstandTotZiekenhuis_11_vrg", 
+#"AfstandTotOpritHoofdverkeersweg_89_vrg", 
+#"Restaurants_3km_vrg", 
+#"AfstandTotBelangrijkOverstapstation_91_vrg", 
+#"AfstandTotKinderdagverblijf_52_vrg", 
+#"AfstandTotBuitenschoolseOpvang_56_vrg", 
+#"prop_nonwest_vrg", 
+#"prop_singles_vrg", 
+#"prop_couples_vrg", 
+#"prop_fam_vrg", 
+#"prop_bijstand_vrg", 
+#"prop_income_socialmin_vrg", 
+#"perc_property_vrg", 
+#"perc_rent_vrg", 
+#"perc_after2000_vrg", 
+#"cluster",
+
+covar_nh <- c(
+            "code",
+            "jaar",
+            "GemiddeldeWoningwaarde_35", 
+            #"Omgevingsadressendichtheid_106", 
+            #"AfstandTotZiekenhuis_11", 
+            #"AfstandTotOpritHoofdverkeersweg_89", 
+            #"Restaurants_3km", 
+            #"AfstandTotBelangrijkOverstapstation_91", 
+            #"AfstandTotKinderdagverblijf_52",
+            #"AfstandTotBuitenschoolseOpvang_56", 
+            "prop_nonwest", 
+            "prop_singles", 
+            #"prop_couples", 
+            #"prop_fam", 
+            "prop_bijstand"
+            #"prop_income_socialmin", 
+            #"perc_property", 
+            #"perc_rent", 
+            #"perc_after2000"
+            )
+
+
+
+
+nhchar_subs <- nhchar %>% 
+  select(covar_nh)
+
+
+
+
+
+##### get data in right shape for ML model
+
+#list of alternatives (neighbourhoods)
+alternatives <- moves_subs %>%
+  distinct(buurtnaam, buurtcode) %>%
+  rename(alternatives = buurtnaam)
+
+
+a <- moves_subs[,.(move_id, jaar, buurtnaam)]
+
+#subset of moves to try approach
+a <- a[move_id %in% seq(33,533,50),]
+
+
+dt <- data.table()
+
+for (row in 1: nrow(a)){
+  temp <- data.table(a[row], alternatives)
+  dt <- rbind(dt, temp)
+}
+
+dt[, choice := ifelse(buurtnaam == alternatives, 1, 0)]
+dt[, buurtnaam := NULL]
+dt_alt <- dt %>%
+  left_join(subs, by = c("move_id", "jaar"), suffix = c("_alt", "_choice")) %>%
+  left_join(nhchar_subs, by = c("buurtcode_alt" = "code", "jaar"))
 
--- src/analysis/nh_types.R
+++ src/analysis/nh_types.R
@@ -4,59 +4,9 @@ library(data.table)
 source("src/data-preparation/clean_cbs_data.R")
 
 #load("gen/analysis/input/moves_db_anonym.Rdata")
-source("src/data-preparation/fill_anonym.R")
-
-####### prepare data for clustering NHs ##########################
-
-moves_db <- moves_db_anonym %>% data.table
+source("src/data-preparation/clean_data_nh.R")
 
 
-#Idenitfy neighbourhood municipalities
-
-moves_db[buurtnaam %in% c("Heusden", "Vught", "Sint Michielsgestel", "Bernheze", "Oss"), `:=` (buurtnaam = "neighbour mun", wijknaam = "neighbour mun")]
-
-moves_db[buurtnaam_vrg %in% c("Heusden", "Vught", "Sint Michielsgestel", "Bernheze", "Oss"), `:=` (buurtnaam_vrg = "neighbour mun", wijknaam_vrg = "neighbour mun")]
-
-#remove cases from outside regio
-moves_db<-moves_db %>%
-  filter(buurtnaam != 'Outside Region') %>%
-  filter(buurtnaam_vrg != "Outside Region")%>%
-  data.table
-
-
-moves_db[gem != "'s-Hertogenbosch" & buurtnaam != "neighbour mun", `:=` (buurtnaam = "outside", wijknaam = "outside")]
-
-moves_db[vrggem != "'s-Hertogenbosch" & buurtnaam_vrg != "neighbour mun", `:=` (buurtnaam_vrg = "outside", wijknaam_vrg = "outside")]
-
-
-# take out neighbourhoods with no living space
-moves_db <- moves_db %>%
-  filter(!buurtnaam %in% c("Bedrijventerrein-Zuid",  "Bedrijventerrein De Herven", "De Lanen", "Zoggel - Berkt", "Kloosterstraat", "De Rietvelden-Oost", 
-                           "Bedrijventerrein Nuland", "Bedrijventerrein Kruisstraat"))
-
-### 2018 #####
-
-moves_db_18 <- filter(moves_db, jaar == 2018)
-
-#merge neighbourhood characteristics for 2018 moving data
-
-moves_db_18 <- moves_db_18 %>%
-  left_join(nhchar_18, by=c("vrg_buurtcode" = "code"))
-
-
-#list of all neighbourhoods with moves
-nh_db <- moves_db_18 %>%
-  filter(!buurtnaam %in% c("neighbour mun", "outside")) %>%
-  distinct(buurtnaam, .keep_all = T) %>%
-  select(c(jaar, buurtnaam, buurtcode)) %>%
-  left_join(nhchar_18, by = c("buurtcode" = "code"))
-
-
-#remove neighbourhoods with missing data
-# !!!! check how to solve this!
-nh_db <- nh_db %>%
-  filter(HuishoudensTotaal_28 > 50) %>%
-  filter(!buurtnaam %in% c("De Lage Kant", "Heeseind", "A2 zone Rosmalen-Zuid")) #take out because of NAs in some variables
 
 ############# NH types #######################
 
