d320b0051ed6a748b35311125445a5ce0945fa44
laura.datta <laura@datta-online.com>
2020-04-12 07:11:21 +0200

    add make file

--- src/data-preparation/fill_anonym.R
+++ src/data-preparation/fill_anonym.R
@@ -12,4 +12,3 @@ moves_db_anonym[, age := sample(seq(18,90),nrow(moves_db_anonym), replace = T)]
 moves_db_anonym[, AANBEW := sample(seq(1:6),nrow(moves_db_anonym), replace = T)]
 moves_db_anonym[, etnikort := factor(sample(seq(1:3),nrow(moves_db_anonym), replace = T), labels = c("dutch", "non_west", "west"))]
 moves_db_anonym[, PRSGES := factor(sample(seq(1:2),nrow(moves_db_anonym), replace = T), labels = c("male", "female"))]
-
