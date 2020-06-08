2a024f92649dd8743e8b76abd910ba0462de4cb1
laura.datta <laura@datta-online.com>
2020-06-08 10:59:10 +0200

    delete unecessary files

--- src/paper/proposals/rendering_markdown.R
+++
@@ -1,9 +0,0 @@
-library(knitr)
-library(rmarkdown)
-
-#source R script for generating objects
-source("src/analysis/map_nh_types.R")
-
-#render markdown
-
-render("src/paper/proposals/nh_types_map_markdown.Rmd", output_dir = "gen/paper/output")
